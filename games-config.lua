27798597680ff2799be2dcacd1818683aab8eb1a
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2025-10-09 21:58:44 +0300

    Update games-config.lua

--- games-config.lua
+++ games-config.lua
@@ -17,5 +17,13 @@ return {
         scriptUrl = "https://raw.githubusercontent.com/nouralddin-abdullah/Plants-Vs-Brainrots/refs/heads/main/main.lua",
         premium = false, -- true = requires key, false = free
         icon = "🌱"
+    },
+    
+    {
+        name = "Basketball:Zero",
+        gameId = 123456789, -- Replace with actual game ID
+        scriptUrl = "https://raw.githubusercontent.com/nouralddin-abdullah/Shittyscripts/refs/heads/main/basketball%3A%20Zero",
+        premium = false, -- true = requires key, false = free
+        icon = "🏀"
     }
 }
