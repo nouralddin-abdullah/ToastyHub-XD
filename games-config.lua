c555c6d055924fb523e2d5fe69b5dd484b415f36
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2025-10-09 15:22:46 +0300

    Update games-config.lua

--- games-config.lua
+++ games-config.lua
@@ -5,7 +5,7 @@
 -- Simply add or remove games from this list
 return {
     {
-        name = "99 Nights",
+        name = "99 Nights (EXECUTE IN FOREST ✅ NOT LOBBY ❌)",
         gameId = 123456789, -- Replace with actual game ID
         scriptUrl = "https://raw.githubusercontent.com/nouralddin-abdullah/99-night/refs/heads/main/main-en-script.lua",
         premium = true, -- true = requires key, false = free
