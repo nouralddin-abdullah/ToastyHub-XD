dd0ccac76eec3a60de0cc3439de7ff05d30d361c
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2025-10-12 03:36:57 +0300

    Update games-config.lua

--- games-config.lua
+++ games-config.lua
@@ -19,5 +19,12 @@ return {
         scriptUrl = "https://raw.githubusercontent.com/nouralddin-abdullah/Shittyscripts/refs/heads/main/basketball%3A%20Zero",
         premium = false, -- true = requires key, false = free
         icon = "🏀"
+    },
+    {
+        name = "Fish It",
+        gameId = 123456789, -- Replace with actual game ID
+        scriptUrl = "https://raw.githubusercontent.com/nouralddin-abdullah/Fish-it/refs/heads/main/main.lua",
+        premium = false, -- true = requires key, false = free
+        icon = "🐟"
     }
 }
