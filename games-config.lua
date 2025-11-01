6d04807a1a11a6eb5128184a93738fdb858730a6
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2025-11-01 15:34:15 +0200

    Update games-config.lua

--- games-config.lua
+++ games-config.lua
@@ -1,5 +1,12 @@
 return {
     {
+        name = "Universal Aimbot - ESP",
+        gameId = 123456789, -- Replace with actual game ID
+        scriptUrl = "https://raw.githubusercontent.com/nouralddin-abdullah/Universal-Aimbot---ESP/refs/heads/main/main.lua",
+        premium = true, -- true = requires key, false = free
+        icon = "♾️"
+    },
+    {
         name = "99 Nights (EXECUTE IN FOREST ✅ NOT LOBBY ❌)",
         gameId = 123456789, -- Replace with actual game ID
         scriptUrl = "https://raw.githubusercontent.com/nouralddin-abdullah/99-night/refs/heads/main/main-en-script.lua",
